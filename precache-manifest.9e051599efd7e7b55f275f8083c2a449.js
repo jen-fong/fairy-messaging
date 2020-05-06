340d40106a5c30b4c56346bdfcdfb5896d9f269e
Jenny Fong <jennyfong1015@gmail.com>
2020-05-06 14:42:29 -0400

    Updates

---
+++ precache-manifest.9e051599efd7e7b55f275f8083c2a449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bc4d6117e10a5e142d6dc3374f85be",
+    "url": "/fairy-messaging/index.html"
+  },
+  {
+    "revision": "d772d0fc5e7403c9a7e7",
+    "url": "/fairy-messaging/static/css/main.7d42bbe4.chunk.css"
+  },
+  {
+    "revision": "48302d27c5dd5e86e45b",
+    "url": "/fairy-messaging/static/js/2.446d98be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fairy-messaging/static/js/2.446d98be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d772d0fc5e7403c9a7e7",
+    "url": "/fairy-messaging/static/js/main.2b122260.chunk.js"
+  },
+  {
+    "revision": "eb6baa8a27b589ec3b1b",
+    "url": "/fairy-messaging/static/js/runtime-main.1c04183f.js"
+  }
+]);
